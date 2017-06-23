2ed1738a88b5e4ab0d4d7d3ac7f96ca423d7e87e
Raza <horrorgreetings@gmail.com>
2017-06-23 18:19:14 +0500

    Emit newMessage on createMessage 2

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+ var socket = io();
+
+ socket.on('connect', function() {
+     console.log('Connected to server');
+
+     //  socket.emit("createMessage", {
+     //      to: 'Raza',
+     //      text: 'Hey. This is Hasan.'
+     //  });
+ });
+
+ socket.on('disconnect', function() {
+     console.log('Disconnected from server');
+ });
+
+ socket.on("newEmail", function(email) {
+     console.log('New email', email);
+ });
+ socket.on('newMessage', (message) => {
+     console.log("Message", message);
+ });
