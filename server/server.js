5faba735b42ba873ce5ea46dfd6a7c202c2a2ef5
Raza <horrorgreetings@gmail.com>
2017-06-23 18:17:23 +0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -26,6 +26,11 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log("createMessage", message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
 
